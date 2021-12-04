c4d011f257b36190df6439d27c2bbefdcfc6dee0
John Hutchinson <johnhutch@swiftkickweb.com>
2021-12-04 15:53:32 -0500

    tried a few more speach types/sentences.

--- espeak-demo.rb
+++ espeak-demo.rb
@@ -10,3 +10,12 @@ end
 # Speaks "YO!"
 speech = ESpeak::Speech.new("YO! Hey there. how are you?")
 speech.speak # invokes espeak
+
+ESpeak::Speech.new("I sounds kind of weird. Weird POPPING noises. Maybe if I Alter the values, I can make it better", 
+                   voice: 'en-us',
+                   pitch: 90, 
+                   speed: 200
+                  ).speak
+
+speech = ESpeak::Speech.new("Hallo Welt", voice: "de")
+# speech.save("hello-de.mp3") # saves the speach as an mp3 instead of speaking it
